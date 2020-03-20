7b6aab745b307cb1fe45a7c89d5e77eeb5711b73
KirillSagach <racedriver791@yandex.ru>
2020-03-20 13:07:35 +0300

    Задание 6.1 Правки

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var MAX_ITEM = 8;
 var checkinMas = ['12: 00', '13: 00', '14: 00'];
 var checkoutMas = ['12: 00', '13: 00', '14: 00'];
 var typeMas = ['palace, flat, house', 'bungalo'];
 var featuresMas = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photosMas = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var newObjects = [];
+var mapItem = document.querySelector('.map');
+var similarPinList = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('button');
 
 var masRandom = function (itemsMas, maxCount) {
   var itemsMasRandom = [];
@@ -17,57 +20,67 @@ var masRandom = function (itemsMas, maxCount) {
   return itemsMasRandom;
 };
 
-var randomMeasure = function (measure) {
-  return Math.random() * (measure - 0) + 0;
+var randomInteger = function (min, max) {
+  // случайное число от min до (max+1)
+  var rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
 };
 
-var i;
-var mapItem = document.querySelector('.map');
-var mapWidth = mapItem.getBoundingClientRect().width;
-var mapHeight = mapItem.getBoundingClientRect().height;
-for (i = 0; i <= 7; i++) {
-  var locationX = randomMeasure(mapWidth);
-  var locationY = randomMeasure(mapHeight);
-  newObjects[i] = {
-    author: {
-      avatar: 'img/avatars/user0' + i + '.png'
-    },
-    offer: {
-      title: 'Заголовок ' + i,
-      adress: locationX + ',' + locationY,
-      price: i * 20,
-      type: typeMas[Math.random() * (3 - 0) + 0],
-      rooms: i,
-      guests: i,
-      checkin: checkinMas[Math.floor(Math.random() * (2 - 0) + 0)],
-      checkout: checkoutMas[Math.floor(Math.random() * (2 - 0) + 0)],
-      features: masRandom(featuresMas, 5),
-      description: 'описание',
-      photos: masRandom(photosMas, 2)
-    },
-    location: {
-      x: Math.random() * (mapWidth - 0) + 0,
-      y: Math.random() * (630 - 130) + 130
-    }
-  };
-}
-
-mapItem.classList.remove('map--faded');
-
-var similarPinList = document.querySelector('.map__pins');
-var pinTemplate = document.querySelector('#pin').content.querySelector('button');
-var fragment = document.createDocumentFragment();
+var generateData = function () {
+  var newPins = [];
+  var mapWidth = mapItem.getBoundingClientRect().width;
+  for (var i = 1; i <= MAX_ITEM; i++) {
+    var locationX = randomInteger(0, mapWidth);
+    var locationY = randomInteger(130, 630);
+    newPins.push({
+      author: {
+        avatar: 'img/avatars/user0' + i + '.png'
+      },
+      offer: {
+        title: 'Заголовок ' + i,
+        adress: locationX + ',' + locationY,
+        price: i * 20,
+        type: typeMas[Math.random() * (3 - 0) + 0],
+        rooms: i,
+        guests: i,
+        checkin: checkinMas[Math.floor(Math.random() * (2 - 0) + 0)],
+        checkout: checkoutMas[Math.floor(Math.random() * (2 - 0) + 0)],
+        features: masRandom(featuresMas, 5),
+        description: 'описание',
+        photos: masRandom(photosMas, 2)
+      },
+      location: {
+        x: locationX,
+        y: locationY
+      }
+    });
+  }
+  return newPins;
+};
 
 var renderPin = function (currentOdject) {
-  var newPin = pinTemplate.cloneNode(true);
-  newPin.querySelector('.map__pin').style = 'left: ' + randomMeasure(mapWidth) + 40 + 'px; top: ' + randomMeasure(mapHeight) + 40 + 'px';
-  newPin.querySelector('.map__pin').src = currentOdject.author.avatar;
-  newPin.querySelector('.map__pin').alt = currentOdject.offer.title;
+  var mapPin = pinTemplate.cloneNode(true);
+  mapPin.style.left = currentOdject.location.x - mapPin.offsetWidth / 2 + 'px';
+  mapPin.style.top = currentOdject.location.y - mapPin.offsetHeight + 'px';
+
+  var pinImg = mapPin.querySelector('img');
+  pinImg.src = currentOdject.author.avatar;
+  pinImg.alt = currentOdject.offer.title;
+
+  return mapPin;
 };
 
-for (i = 0; i <= newObjects.length; i++) {
-  fragment.appendChild(renderPin(newObjects[i]));
-}
+var renderPins = function (newPins) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < newPins.length; i++) {
+    fragment.appendChild(renderPin(newPins[i]));
+  }
+  similarPinList.appendChild(fragment);
+};
 
-similarPinList.appendChild(fragment);
-
+var init = function () {
+  mapItem.classList.remove('map--faded');
+  var data = generateData();
+  renderPins(data);
+};
+init();
